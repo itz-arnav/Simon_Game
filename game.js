02fe3fc84021245f7807f8c402a8deae8db466e7
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-12-15 19:47:03 +0530

    Files Added

---
+++ game.js
@@ -0,0 +1,73 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkanswer(userClickedPattern.length);
+});
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkanswer(current_level){
+      if(userClickedPattern[current_level-1] == gamePattern[current_level-1]){
+            
+            if(userClickedPattern.length == gamePattern.length){
+                  userClickedPattern = []
+                  setTimeout(nextSequence(), 5000);
+            }
+      }
+      else{
+
+            playSound("wrong");
+            $("body").toggleClass("red");
+            setTimeout(() => {
+                  $("body").toggleClass("red");
+            }, 100);
+
+            userClickedPattern = []
+            gamePattern = []
+            level = 0;
+            $("#level-title").text("Game Over, Press Any Key to Restart");
+            started = false;
+      }
+}
